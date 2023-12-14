e3489f9171ad8f4e012093eeaedfdacd18e4d260
wasd1g <koki.karadjov@gmail.com>
2023-12-14 22:42:00 +0200

    vsichko e mnogo precakano i ne bachka...

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
-import NotesView from "./notesView.js"
-import NotesAPI from "./notesAPI.js"
+import App from "./App.js";
 
-const app = document.getElementById("app");
-const view = new NotesView(app, {
-    onNoteAdd() {
-        console.log("Adding a new note!");
-        
-        
-    },
-    onNoteSelect(id) {
-        console.log("Note Selected: " + id)
-    },
-    OnNoteDelete(id) {
-        console.log("Note deleted!" + id);
-    }, 
-
-    onNoteEdit(newTitle, newBody) {
-        console.log(newTitle);
-        console.log(newBody);
-    },
-});
-
-const notes = NotesAPI.getAllNotes();
-
-view.updateNotesList(notes);
-view.updateActiveNote(notes[0]);
+const root = document.getElementById("app");
+const app = new App(root);
--- js/notesView.js
+++ js/notesView.js
@@ -1,20 +1,19 @@
 export default class NotesView {
-    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, OnNoteDelete } = {}) {
+    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
         this.root = root;
         this.onNoteSelect = onNoteSelect;
         this.onNoteAdd = onNoteAdd;
         this.onNoteEdit = onNoteEdit;
-        this.OnNoteDelete = OnNoteDelete;
+        this.onNoteDelete = onNoteDelete;
         this.root.innerHTML = `
-        <div class="notes_sidebar">
-        <button class="notes_add" type="button">Add New Note</button>
-        <div class="notes_list">
-        </div>
-        </div>
-        <div class="notes_preview">
-            <input type="text" class="notes_title" placeholder="New Note">
-            <textarea class="notes_body">Take a note...</textarea>
-        </div>
+            <div class="notes_sidebar">
+                <button class="notes_add" type="button">Add Note</button>
+                <div class="notes_list"></div>
+            </div>
+            <div class="notes_preview">
+                <input class="notes_title" type="text" placeholder="New Note...">
+                <textarea class="notes_body">Take Note...</textarea>
+            </div>
         `;
 
         const btnAddNote = this.root.querySelector(".notes_add");
@@ -30,37 +29,34 @@ export default class NotesView {
                 const updatedTitle = inpTitle.value.trim();
                 const updatedBody = inpBody.value.trim();
 
-                this.onNoteEdit(updatedTitle, updatedBody)
-            })
-        })
-
-        console.log(this._createListItemHTML(300, "New note", "Nov note!", new Date()));
+                this.onNoteEdit(updatedTitle, updatedBody);
+            });
+        });
 
         this.updateNotePreviewVisibility(false);
     }
 
-    // private method _
-    _createListItemHTML (id, title, body, updated) {
+    _createListItemHTML(id, title, body, updated) {
         const MAX_BODY_LENGTH = 60;
 
         return `
             <div class="notes_list-item" data-note-id="${id}">
                 <div class="notes_small-title">${title}</div>
-                    <div class="notes_small-body">
-                        ${body.substring(0, MAX_BODY_LENGTH)}
-                        ${body.length > MAX_BODY_LENGTH ? "..." : ""}
-                    </div>
-                    <div class="notes_small-updated">
-                        ${updated.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
+                <div class="notes_small-body">
+                    ${body.substring(0, MAX_BODY_LENGTH)}
+                    ${body.length > MAX_BODY_LENGTH ? "..." : ""}
                 </div>
-             </div>
-                 `;
+                <div class="notes_small-updated">
+                    ${updated.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
+                </div>
+            </div>
+        `;
     }
 
     updateNotesList(notes) {
         const notesListContainer = this.root.querySelector(".notes_list");
 
-        //Empty list
+        // Empty list
         notesListContainer.innerHTML = "";
 
         for (const note of notes) {
@@ -70,7 +66,6 @@ export default class NotesView {
         }
 
         // Add select/delete events for each list item
-
         notesListContainer.querySelectorAll(".notes_list-item").forEach(noteListItem => {
             noteListItem.addEventListener("click", () => {
                 this.onNoteSelect(noteListItem.dataset.noteId);
@@ -80,7 +75,7 @@ export default class NotesView {
                 const doDelete = confirm("Are you sure you want to delete this note?");
 
                 if (doDelete) {
-                    this.OnNoteDelete(noteListItem.dataset.noteId);
+                    this.onNoteDelete(noteListItem.dataset.noteId);
                 }
             });
         });
